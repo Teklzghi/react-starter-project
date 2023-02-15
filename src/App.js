34b8ed9c5ec0cdb1a93dc56039e4c4547705f9e3
Teklzghi <tekitedro9@gmail.com>
2023-02-15 20:09:05 +0100

    Use arrow functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import showsData from  './shows-data'
 
+function Show (props){
+  return (
+    <div className='Show'>
+      <img 
+      className='Show-image'
+      src={ props.showObject.image.medium } 
+      alt={ props.showObject.name } />
+    <h2>{ props.showObject.name }</h2>
+   </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
-      <h1>The Best TV Shows App</h1>
-    </div>
+      <h1>The Best Tv Show App</h1>
+       { showsData.map(X => <Show showObject={x} />) }
+      </div>
   );
 }
 
